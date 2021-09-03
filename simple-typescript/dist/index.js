26e0d86cf4357e2d6470075b575b50db9d9382e6
tonidelgadomadrona <50912526+tonidelgadomadrona@users.noreply.github.com>
2021-09-03 09:01:05 +0200

    Created the pizza-project and remove it the simple-typescript files

--- simple-typescript/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function multiply(a, b) {
-    return a * b;
-}
-const a = 8;
-const b = 2;
-console.log(`${a} * ${b} = ${multiply(a, b)}`);
--- simple-typescript/src/index.ts
+++
@@ -1,8 +0,0 @@
-function multiply(a: number, b: number): number {
-  return a * b;
-}
-
-const a = 8;
-const b = 2;
-
-console.log(`${a} * ${b} = ${multiply(a, b)}`);
